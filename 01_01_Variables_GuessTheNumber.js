a53df4c808519db77378b1806a938dbf5b23a697
Meija Lohiniva <mlohiniva@hotmail.com>
2021-08-29 21:06:07 +0300

    Added 01_03

--- 01_01_Variables_GuessTheNumber.js
+++ 01_01_Variables_GuessTheNumber.js
@@ -2,8 +2,7 @@
 //  Modified from Game.js (c) Kari Laitinen http://www.naturalprogramming.com
 
 // Here we print out a message
-process.stdout.write( "\n This program is a computer game. Please, "
-                    + "\n type in a number : " ) ;
+process.stdout.write( "\n This program is a computer game. Please, " + "\n type in a number : " ) ;
 
 // Here we create an event handler to listen to on event of stdin
 // inputFromUser carries the input given by the user
@@ -18,9 +17,7 @@ process.stdin.on( "data", function( inputFromUser )
    const SECRET_NUMBER = givenNumber + 1;
 
    // Let's print out the message about losing/winning
-   process.stdout.write( "\n You typed in " + givenNumber + "."
-                      +  "\n My number is " + SECRET_NUMBER  + "."
-                      +  "\n Sorry, you lost. I won. The game is over.\n\n" ) ;
+   process.stdout.write( "\n You typed in " + givenNumber + "." +  "\n My number is " + SECRET_NUMBER  + "." +  "\n Sorry, you lost. I won. The game is over.\n\n" ) ;
 
    process.exit() ;
 
---
+++ 01_03_Operators_DistanceConverter2.js
@@ -0,0 +1,46 @@
+//  Modified from Distance.js (c) Kari Laitinen  http://www.naturalprogramming.com
+
+// This creates a constant of utilities module
+const util = require( "util" ) ; 
+
+// Print out a message asking for user input
+process.stdout.write( "\n This program converts meters to other units of" +  "\n distance. Please, enter a distance in meters:  " ) ;
+
+// Create an event handler to listen for on event for stdin
+// User input will be carried in inputFromUser
+process.stdin.on( "data", function( inputFromUser )
+{
+       // The statements below will be executed automatically after
+       // the user has typed in the value.
+
+       // We want to convert the input to number so that we can use it for calculations
+       let distanceInMeters = Number( inputFromUser.toString() );
+
+       // Let's create constants for conversion rates
+       const METERS_TO_KILOMETERS = 1000;
+       const METERS_TO_MILES = 6.21371e-4;
+       const METERS_TO_YARDS = 1.093613;
+       const METERS_TO_FEET = 3.280840;
+       const FEET_TO_INCHES = 12;
+       const METERS_TO_LIGHTYEARS = 2.99793e8 * 365 * 24 * 3600;
+
+       let distanceInKilometers = distanceInMeters / METERS_TO_KILOMETERS;
+       let distanceInMiles = METERS_TO_MILES * distanceInMeters;
+       let distanceInYards = METERS_TO_YARDS * distanceInMeters;
+       let distanceInFeet = METERS_TO_FEET * distanceInMeters;
+       let distanceInInches = FEET_TO_INCHES * distanceInFeet;
+       let distanceInLightYears = distanceInMeters / ( METERS_TO_LIGHTYEARS );
+
+       process.stdout.write( "\n " + distanceInMeters + " meters is: \n\n" );
+
+       process.stdout.write(util.format( "\t%s kilometers\n", distanceInKilometers.toFixed(3)));
+       process.stdout.write(util.format( "\t%s miles \n", distanceInMiles.toFixed(3)));
+       process.stdout.write(util.format( "\t%s yards \n", distanceInYards.toFixed(3)));
+
+       process.stdout.write(util.format( "\t%s feet  \n\t%s  inches \n", distanceInFeet.toFixed(3), distanceInInches.toFixed(3)));
+
+       process.stdout.write(util.format( "\t%s light years \n\n", Number.parseFloat(distanceInLightYears).toFixed(3)));
+
+       process.exit() ;
+} ) ;
+
